1c2249e968876a3f27748b1684886c474c382c9f
jmd_10 <jason@evolo.xyz>
2020-05-26 13:48:58 -0400

    Initial commit

---
+++ mongodb_api.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+from pprint import pprint  # pprint library is used to make the output look more pretty
+
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient(<<MONGODB URL>>)
+db=client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
---
+++ scrap.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+IEX_API_Key = 'pk_136710d08e6d4a93974afa191926dab0'
+
+tickers = ['JPM', 'BAC', 'C', 'WFC', 'GS',]
+
+#Create an empty string called `ticker_string` that we'll add tickers and commas to
+ticker_string = ''
+
+#Loop through every element of `tickers` and add them and a comma to ticker_string
+for ticker in tickers:
+    ticker_string += ticker
+    ticker_string += ','
+
+#Drop the last comma from `ticker_string`
+ticker_string = ticker_string[:-1]
+
+#Create the endpoint and years strings
+endpoints = 'chart'
+years = '5'
+
+#Interpolate the endpoint strings into the HTTP_request string
+HTTP_request = f'https://cloud.iexapis.com/stable/stock/market/batch?symbols={ticker_string}&types={endpoints}&range={years}y&cache=true&token={IEX_API_Key}'
+
+#Send the HTTP request to the IEX Cloud API and store the response in a pandas DataFrame
+bank_data = pd.read_json(HTTP_request)
+
+#Create an empty list that we will append pandas Series of stock price data into
+series_list = []
+
+#Loop through each of our tickers and parse a pandas Series of their closing prices over the last 5 years
+for ticker in tickers:
+    series_list.append(pd.DataFrame(bank_data[ticker]['chart'])['close'])
+
+#Add in a column of dates
+series_list.append(pd.DataFrame(bank_data['JPM']['chart'])['date'])
+
+#Copy the 'tickers' list from earlier in the script, and add a new element called 'Date'.
+#These elements will be the column names of our pandas DataFrame later on.
+column_names = tickers.copy()
+column_names.append('Date')
+
+#Concatenate the pandas Series together into a single DataFrame
+bank_data = pd.concat(series_list, axis=1)
+
+#Name the columns of the DataFrame and set the 'Date' column as the index
+bank_data.columns = column_names
+bank_data.set_index('Date', inplace = True)
+
+# print(bank_data)
